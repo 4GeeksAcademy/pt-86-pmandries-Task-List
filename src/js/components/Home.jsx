d2be21aa2c3252f61d657e23de2cc4e9f956b6a4
pmandries <pandries@aol.com>
2025-10-30 01:14:59 +0000

    first commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	
+	const [tasks, setTasks] = useState([]);
+	const [userInput, setUserInput] = useState("");
+	const addToList = (task) => {
+		let newTask = {label: task, isDone: false}
+		setTasks([...tasks, newTask]);
+		setUserInput("");
+	}
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container bg-light">
+			<div className = "text-center">
+				<h1>Task List</h1>
+			</div>
+			<div className = "text-center mb-3">
+				<input onChange = {(e) => setUserInput(e.target.value)} value = {userInput} type = "text" placeholder = "Add a task here" />
+				<button onClick = {() => addToList(userInput)}>Add to List</button>
+			</div>
+			<ul>
+				{tasks.map((task, index) => 
+//					<li key = {index}>{task.label}</li>
+					<div key = {index} className="form-check fs-3">
+  						<input className="form-check-input" type="checkbox" value="" id="checkDefault" />
+  						<label className="form-check-label" htmlFor="checkDefault">
+    						{task.label}
+  						</label>
+					</div>
+					)
+				}
+			</ul>
+			
 		</div>
 	);
 };
